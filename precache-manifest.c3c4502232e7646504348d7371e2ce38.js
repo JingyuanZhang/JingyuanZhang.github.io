3b51cb31d8d9da575a7b3e9ccbcc272fb04e62d4
zhangjingyuan02 <zhangjingyuan02@baidu.com>
2019-05-19 17:02:29 +0800

    test pwa

---
+++ precache-manifest.c3c4502232e7646504348d7371e2ce38.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d83d5cbdde2d6669d9d6",
+    "url": "/js/app.438890b0.js"
+  },
+  {
+    "revision": "d26ad9b6b2d9f23f76dc",
+    "url": "/js/chunk-vendors.5101329d.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b71a7a7d13a6a74d8996b9e2a50ad24d",
+    "url": "/index.html"
+  }
+];
