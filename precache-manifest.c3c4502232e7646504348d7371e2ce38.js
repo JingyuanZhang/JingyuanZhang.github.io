65d9af9750fbeb93db498d21bae3efea39944ba7
zhangjingyuan02 <zhangjingyuan02@baidu.com>
2019-05-19 18:00:12 +0800

    change cache file type

--- precache-manifest.c3c4502232e7646504348d7371e2ce38.js
+++ precache-manifest.c3c4502232e7646504348d7371e2ce38.js
@@ -10,9 +10,5 @@ self.__precacheManifest = [
   {
     "revision": "82b9c7a5a3f405032b1db71a25f67021",
     "url": "/img/logo.82b9c7a5.png"
-  },
-  {
-    "revision": "b71a7a7d13a6a74d8996b9e2a50ad24d",
-    "url": "/index.html"
   }
 ];
